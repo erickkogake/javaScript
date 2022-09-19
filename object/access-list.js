ff82f87a3e4b27b32d81f0db4f2ef7b57fc8d5a5
Erick Kogake <erick.kogake@aigroup.local>
2022-09-19 16:57:17 -0300

    objects

---
+++ object/access-list.js
@@ -0,0 +1,21 @@
+const clientes = {
+    name: 'Jo',
+    idade: 35,
+    cpf: '43240699811'
+}
+
+// clientes.forEach(element => {
+//     console.log(element)
+// });
+
+// clientes.map(element => {
+//     console.log(element)
+// })
+
+const array = ["name", "idade","cpf"]
+array.forEach(element => console.log(clientes[element]))
+
+
+
+
+
---
+++ object/acess.js
@@ -0,0 +1,7 @@
+const clientes = {
+    name: 'João',
+    idade: 35,
+    CPF: '43270698845',
+}
+
+console.log(clientes.name, clientes.CPF)
